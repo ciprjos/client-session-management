ea6b188a8ac02f2212f0ae07488861cbd3bac16e
Jose Japhet <josejaphet.cipriano@outlook.com>
2025-11-08 03:36:22 +0800

    Added base entity

---
+++ ClientSessionManagement/Domain/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Common;
+public abstract class BaseEntity<T>
+{
+    public T Id { get; set; } = default!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; } = null;
+}

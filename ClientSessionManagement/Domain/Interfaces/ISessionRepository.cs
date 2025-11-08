8c2937967b888bedb98903f2f775e82e8021b682
Jose Japhet <josejaphet.cipriano@outlook.com>
2025-11-08 18:35:40 +0800

    Added Session repository for Client session

---
+++ ClientSessionManagement/Domain/Interfaces/ISessionRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces;
+public interface ISessionRepository
+{
+    Task AddAsync(Session session, CancellationToken cancellationToken);
+    Task<Session?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<Session?> GetByNameAsync(string name, CancellationToken cancellationToken);
+    Task<IEnumerable<Session>> GetAllAsync(CancellationToken cancellationToken);
+    void Update(Session session);
+}
---
+++ ClientSessionManagement/Infrastructure/Repositories/SessionRepository.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+internal sealed class SessionRepository : ISessionRepository
+{
+    private readonly ApplicationDbContext _context;
+    public SessionRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Session session, CancellationToken cancellationToken)
+    {
+        await _context.Sessions.AddAsync(session, cancellationToken);
+    }
+    public async Task<IEnumerable<Session>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await _context.Sessions.ToListAsync(cancellationToken);
+    }
+    public async Task<Session?> GetByNameAsync(string name, CancellationToken cancellationToken)
+    {
+        return await _context.Sessions.FirstOrDefaultAsync(s => s.Notes == name, cancellationToken);
+    }
+    public async Task<Session?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _context.Sessions.FirstOrDefaultAsync(s => s.Id == id, cancellationToken);
+    }
+    public void Update(Session session)
+    {
+        _context.Sessions.Update(session);
+    }
+}

dc1d624507639e2647e0dfc8be8f1a5ab9534539
Jose Japhet <josejaphet.cipriano@outlook.com>
2025-11-08 15:05:41 +0800

    Added DbContext

---
+++ ClientSessionManagement/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Provider> Providers { get; set; }
+    public DbSet<SessionType> SessionTypes { get; set; }
+    public DbSet<ProviderSessionType> ProviderSessionTypes { get; set; }
+    public DbSet<Session> Sessions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
